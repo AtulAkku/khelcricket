08f5e5fab20093da228d2f1be210026904d8db99
Atul <atulakku99@gmail.com>
2024-03-09 12:34:43 +0530

    atul090324

--- src/Componet/VenueDetails.js
+++ src/Componet/VenueDetails.js
@@ -2,7 +2,27 @@ import React from 'react'
 
 const VenueDetails = () => {
   return (
-    <div>VenueDetails</div>
+    <>
+      <div className="container p-4 g-0 shadow mx-auto my-4 bg-light rounded row ">
+        <div className='col' ><img className='img-fluid rounded' src='../../img/location/mcc.png' alt='' /></div>
+        <div className='col d-flex align-item-center flex-column justify-content-center p-2'>
+          <div className='text-center h1 m-0 p-3 rounded' >Venue Details</div>
+          <h5 className="card-title text-center">Card title</h5>
+          <p>Description:</p>
+                  <p className="card-text text-center">Location: Gurugram</p>
+          <form className='p-3'>
+            <div className="form-group">
+            <label htmlFor="title">Select Date</label>
+            <input type="date" className="form-control" id="dueDate" /*min={today}*/ onChange={(e) => { /*setDueDate(e.target.value)*/ }} />
+          </div>
+            <div className="d-flex justify-content-center mt-2">
+              <button type="button" className="btn bg-nav px-5 fs-6">Check availability!</button>
+              <button type="button" className="btn bg-nav px-5 fs-6">Book Now!</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Componet/Venues.js
+++ src/Componet/Venues.js
@@ -3,25 +3,79 @@ import React from 'react'
 const Venues = () => {
   return (
     <>
-      <div class="container">
-        <div class="row">
-          <div class="col-2">
-            Column
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+          <a href="./venueDetails" className="btn">
+              <div className="card w-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title text-center">Card title</h5>
+                  <p className="card-text text-center">Location: Gurugram</p>
+                  
+                </div>
+              </div>
+            </a>
           </div>
-          <div class="col-2">
-            Column
+          <div className="col-3">
+            <a href="./venueDetails" className="btn">
+              <div className="card w-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title text-center">Card title</h5>
+                  <p className="card-text text-center">Location: Gurugram</p>
+                  
+                </div>
+              </div>
+            </a>
           </div>
-          <div class="col-2">
-            Column
+          <div className="col-3">
+          <a href="./venueDetails" className="btn">
+              <div className="card w-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title text-center">Card title</h5>
+                  <p className="card-text text-center">Location: Gurugram</p>
+                  
+                </div>
+              </div>
+            </a>
           </div>
-          <div class="col-2">
-            Column
+          <div className="col-3">
+          <a href="./venueDetails" className="btn">
+              <div className="card w-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title text-center">Card title</h5>
+                  <p className="card-text text-center">Location: Gurugram</p>
+                  
+                </div>
+              </div>
+            </a>
           </div>
-          <div class="col-2">
-            Column
+          <div className="col-3">
+          <a href="./venueDetails" className="btn">
+              <div className="card w-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title text-center">Card title</h5>
+                  <p className="card-text text-center">Location: Gurugram</p>
+                  
+                </div>
+              </div>
+            </a>
           </div>
-          <div class="col-2">
-            Column
+          <div className="col-3">
+          <a href="./venueDetails" className="btn">
+              <div className="card w-100">
+                <img src="..." className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title text-center">Card title</h5>
+                  <p className="card-text text-center">Location: Gurugram</p>
+                  
+                </div>
+              </div>
+            </a>
           </div>
         </div>
       </div>
