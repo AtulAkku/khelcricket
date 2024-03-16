673574a9ae805d8b79e8e75a8d99317ff46b971e
Atul <atulakku99@gmail.com>
2024-03-16 09:30:50 +0530

    160324 09:30

--- src/Componet/Error404.js
+++ src/Componet/Error404.js
@@ -5,7 +5,7 @@ const Error404 = () => {
   return (
     <div className='bg-light px-5'>
       <div className='row'>
-        <div className="col col-12 col-lg-6 text-center"><img className="img-fluid w-75 p-5" src="/img/404.png" alt="Error 404" /></div>
+        <div className="col col-12 col-lg-6 text-center"><img className="img-fluid w-75 p-5" src="/img/404-1.png" alt="Error 404" /></div>
         <div className="col col-12 col-lg-6 my-auto p-5">
           <h1 className='text-center display-3 fw-bold'>Oops! Page not found.</h1>
           <div className="text-center">
